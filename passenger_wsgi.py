fe9fe1acd847de52ff4ef70ba842e83f070ce667
TutajITeraz <41327134+TutajITeraz@users.noreply.github.com>
2025-11-13 13:59:59 +0100

    wsgi update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,19 @@
-import imp
 import os
 import sys
 
+# Limit OpenBLAS to use 2 threads
+os.environ["OPENBLAS_NUM_THREADS"] = "2"
 
-sys.path.insert(0, os.path.dirname(__file__))
 
-wsgi = imp.load_source('wsgi', 'run_server.sh')
-application = wsgi.application
+# full path to directory that contains manage.py
+PROJECT_DIR = "/home2/reboldho/domains/monumenta-poloniae-liturgica.ispan.pl/ritus"
+
+# add to sys.path
+sys.path.insert(0, PROJECT_DIR)
+
+# name of the folder with settings.py (replace XXX with the real name, e.g. "ritus" or "monumenta")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ritus_indexer.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
